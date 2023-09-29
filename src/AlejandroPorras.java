8e48e00610f82fb5ec68a69798388783915bcf64
Alejandro-Porras-Sanchez <alejandroporras@iesmre.com>
2023-09-29 17:55:40 +0200

    Primer commit

---
+++ src/AlejandroPorras.java
@@ -0,0 +1,5 @@
+public class AlejandroPorras {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Quién esté libre de pecado que enseñe su historial de navegación. ");
+    }
+}
